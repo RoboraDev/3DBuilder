e198aafe6568df4638927c75869635e2bfcb9cd4
Dmitriy Molchanov <d.molchanov.dev@gmail.com>
2025-09-03 19:00:57 +0200

    Replaced react-scripts by vite

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import RobotBuilder from './components/RobotBuilder';
 function App() {
   return (
     <div className="App">
-      <header className="text-3xl text-center font-bold">3D Builder</header>
+      <header className="text-4xl text-center font-bold">3D Builder</header>
       {/* Load the robot URDF file into our RobotBuilder */}
       <RobotBuilder url="/urdf/cassie/cassie.urdf"/>
     </div>
