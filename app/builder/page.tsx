d64509e416b8e05c629cd765aaf3f05827d5fa1a
Dmitriy Molchanov <d.molchanov.dev@gmail.com>
2025-09-04 02:49:40 +0200

    Added next.js  framework

--- app/builder/page.tsx
+++ app/builder/page.tsx
@@ -1,13 +1,15 @@
-import RobotBuilder from './components/RobotBuilder';
+"use client";
 
-function App() {
+import RobotBuilder from "./components/RobotBuilder";
+
+function Builder() {
   return (
     <div className="App">
       <header className="text-4xl text-center font-bold">3D Builder</header>
       {/* Load the robot URDF file into our RobotBuilder */}
-      <RobotBuilder url="/urdf/cassie/cassie.urdf"/>
+      <RobotBuilder url="/urdf/cassie/cassie.urdf" />
     </div>
   );
 }
 
-export default App;
+export default Builder;
